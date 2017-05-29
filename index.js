0e14caddbbae98676d21f1e231b5b3a9fc08d32e
allain <allain.lalonde@favequest.com>
2017-05-29 16:46:31 -0400

    removing unused code

--- index.js
+++ index.js
@@ -31,52 +31,9 @@ function transpile (piff) {
     new ScopeVisitor(),
     new ParentCallVisitor(),
     new PHPGeneratorVisitor()
-    /* new Visitor({
-      pre: {
-        '*': n => {
-          // console.log(n)
-        }
-      }
-    }) */
   ]
 
   visitors.forEach(v => v.visitTree(parseTree))
 
-  let php = parseTree.php
-  // .replace(/;;+/g, ';')
-  // .replace(/;\n;\n/, ';\n')
-  // .replace(/;\)/g, ')')
-  // .replace(/;,/g, ',')
-  // .replace(/;]/g, ']')
-  // .replace(/; ./g, '.')
-  // .replace(/;->/g, '->')
-  // .replace(/;::/g, '::')
-  // .replace(/\}(\s)?(\n\s*)\}/, '}$2}') // remove line after last method declaration and end of class spec`
-
-  // return autoIndent(php).replace(/\n\n\n+/g, '\n\n').replace(/\{\s+\}/g, '{}')
-  return php
-}
-
-function addIndent (line, indents) {
-  return line.trim() ? Array(indents + 1).join('  ') + line.trim() : ''
-}
-
-function autoIndent (php) {
-  let indent = 0
-  return php
-    .split('\n')
-    .map(l => {
-      if (l.match(/^\s*}.*{\s*$/)) {
-        return addIndent(l, indent - 1)
-      } else if (l.match(/{\s*$/)) {
-        return addIndent(l, indent++)
-      } else if (l.match(/}(\s*;)?\s*$/)) {
-        return addIndent(l, --indent)
-      } else if (l.match(/^\s*}/)) {
-        return addIndent(l, --indent)
-      } else {
-        return addIndent(l, indent)
-      }
-    })
-    .join('\n')
+  return parseTree.php
 }
